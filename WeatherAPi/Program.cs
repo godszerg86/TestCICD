98e9e6305d6f9649f1d60cf9a24e42e045390251
Sergei Belialov <sbelialov@rrc.ca>
2025-12-10 13:17:20 -0600

    new weather

--- WeatherAPi/Program.cs
+++ WeatherAPi/Program.cs
@@ -24,7 +24,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild"
 };
 
 
